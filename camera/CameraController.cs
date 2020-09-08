da9f354d8ca57bdeb96c0c93d5d6e89e85852e57
trommlbomml <peter_friedland@gmx.de>
2020-09-08 17:41:33 +0200

    prepared build mode view

--- camera/CameraController.cs
+++ camera/CameraController.cs
@@ -3,14 +3,19 @@ using System;
 
 public class CameraController : Spatial
 {
-    private const float AnimationSpeed = 0.5f;
-    private const float AnimationSpeedTargetHit = 0.25f;
+    [Export] public float AnimationSpeedCannon { get; set; } = 0.5f;
+    [Export] public float AnimationSpeedTargetHit { get; set; } = 0.25f;
     
     private static readonly Vector3 DistanceToHitTarget = new Vector3(1, 1, 8);
     private static readonly Vector3 DistanceToWeapon = new Vector3(0.4f, 0.5f, 0.8f);
 
+    private static readonly Vector3 DistanceToCastle = new Vector3(0, 0, 20);
+
+    private Castle _castle;
     private IWeapon _attachedWeapon;
     private Tween _tween;
+
+    private Spatial _rotatorX;
     private Camera _camera;
 
     public bool IsAnimating { get; private set; }
@@ -18,13 +23,25 @@ public class CameraController : Spatial
     public override void _Ready()
     {
         _tween = GetNode<Tween>("Tween");
-        _camera = GetNode<Camera>("Camera");
+        _camera = GetNode<Camera>("rotator_x/Camera");
+        _rotatorX = GetNode<Spatial>("rotator_x");
     }
 
     public override void _Process(float delta)
     {
-        if (_attachedWeapon == null) return;
+        if (_attachedWeapon != null)
+        {
+            HandleAttachedToWeapon();
+        }
         
+        if (_castle != null)
+        {
+            HandleCastleMode();
+        }
+    }
+
+    private void HandleAttachedToWeapon()
+    {
         if (!IsAnimating)
         {
             Rotation = new Vector3(0, _attachedWeapon.RotationY, 0);
@@ -41,6 +58,11 @@ public class CameraController : Spatial
         }
     }
 
+    private void HandleCastleMode()
+    {
+
+    }
+
     private void AnimateToTargetPosition(Vector3 position)
     {
         _camera.Translation = DistanceToHitTarget;
@@ -53,14 +75,15 @@ public class CameraController : Spatial
     {
         _camera.Translation = DistanceToWeapon;
         IsAnimating = true;
-        _tween.InterpolateProperty(this, "translation", Translation, _attachedWeapon.Position, AnimationSpeed, Tween.TransitionType.Linear, Tween.EaseType.In);
-        _tween.InterpolateProperty(this, "rotation", Rotation, new Vector3(0, _attachedWeapon.RotationY, 0), AnimationSpeed, Tween.TransitionType.Linear, Tween.EaseType.In);
+        _tween.InterpolateProperty(this, "translation", Translation, _attachedWeapon.Position, AnimationSpeedCannon, Tween.TransitionType.Linear, Tween.EaseType.In);
+        _tween.InterpolateProperty(this, "rotation", Rotation, new Vector3(0, _attachedWeapon.RotationY, 0), AnimationSpeedCannon, Tween.TransitionType.Linear, Tween.EaseType.In);
         _tween.Start();
     }
 
     public void AttachTo(IWeapon weapon, bool animate)
     {
         _attachedWeapon = weapon;
+        _castle = null;
         _camera.Translation = DistanceToWeapon;
         if (animate)
         {
@@ -70,6 +93,14 @@ public class CameraController : Spatial
         {
             Translation = _attachedWeapon.Position;
         }
+    }
+
+    public void SwitchToCastle(Castle castle)
+    {
+        _attachedWeapon = null;
+        _castle = castle;
+
+        _camera.Translation = DistanceToCastle;
     }
 
     public void OnTweenAllCompleted()
